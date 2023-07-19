d7039dabd1b5c86963635c5d89ba085c37c9294d
tannghe07 <tannghe07@gmail.com>
2023-07-19 20:42:46 +0700

    create migration and models

--- my-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ my-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin');
+            $table->boolean('is_active');
             $table->rememberToken();
             $table->timestamps();
         });
